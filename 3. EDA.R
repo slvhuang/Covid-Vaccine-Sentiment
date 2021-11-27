bb046d0074bb002636af851573ff5845a719a1a7
Silvia <status>
2021-11-26 21:54:55 -0800

    Reorganized + EDA

--- 3. EDA.R
+++ 3. EDA.R
@@ -1,11 +1,24 @@
 # Load the libraries
 library(tidyverse)
 library(lubridate)
+library(maps)
+library(ggthemes)
+
 
 # Load saved dataset
 tweets_sentiment <- read.csv("tweets_sentiment.csv", header = TRUE)
 tweets_sentiment$date <- as.Date(tweets_sentiment$date)
 summary(tweets_sentiment)
+cities <- read.csv("uscities.csv", header = TRUE)
+
+us_states <- map_data("state")
+state_names <- cities %>% select(state_id, state_name) %>% distinct()
+  
+tweets_sentiment <- merge(tweets_sentiment, state_names, by.x = "user_state", 
+                          by.y = "state_id")
+
+tweets_sentiment$state_name <- tolower(tweets_sentiment$state_name)
+
 
 # Split data before/after the start of vaccination
 before_vac <- tweets_sentiment[tweets_sentiment$admin_per_100k == 0,]
@@ -15,20 +28,86 @@ after_vac <- tweets_sentiment[tweets_sentiment$admin_per_100k != 0,]
 # Distribution of tweets collected by state
 ggplot(tweets_sentiment, aes(x = fct_infreq(user_state))) + 
   geom_bar() + 
-  labs(title = "Tweet Users by State", x = "State", y = "Count")
+  labs(title = "Tweets Before Vaccination by State ", x = "State", y = "Count") +
+  coord_flip() +
+  theme_light()
 
 # Sentiment by state before vaccination
-ggplot(before_vac, aes(x = factor(user_state), y = sentiment)) + geom_boxplot()
-senti_state_before <- aggregate(before_vac$sentiment, list(before_vac$user_state), mean)
+ggplot(before_vac, aes(x = reorder(factor(user_state), sentiment, FUN = median), y = sentiment)) + 
+  geom_boxplot() +
+  coord_flip() +
+  theme_light() +
+  labs(title = "Sentiments Before Vaccination by State ", x = "State", y = "Count")
+  
+senti_state_before <- before_vac %>% 
+  group_by(user_state) %>%
+  summarize(mean_sentiment = mean(sentiment),
+            n =  n()) %>%
+  merge(state_names, by.x = "user_state", 
+        by.y = "state_id")
+
+senti_state_before$state_name <- tolower(senti_state_before$state_name)
+
+before_vac %>% 
+  group_by(date) %>%
+  summarize(mean_sentiment = mean(sentiment), n =  n()) %>%
+  ggplot(aes(x = date, y = mean_sentiment)) + 
+  geom_point() +
+  geom_smooth(span = 0.4)
+
+us_senti_before <- left_join(senti_state_before, us_states, by = c("state_name" = "region"))
+
+ggplot(data = us_senti_before,
+       mapping = aes(x = long, y = lat, group = group, fill = mean_sentiment)) +
+  geom_polygon(color = "gray90", size = 0.1) +
+  coord_map(projection = "albers", lat0 = 39, lat1 = 45) + 
+  scale_fill_gradient2() +
+  labs(title = "Sentiment Before Vaccination") + 
+  theme_map() + 
+  labs(fill = "Sentiment")
+
+ggsave("sentiment_before_vaccination.png")
 
 # Sentiment by state after vaccination
-ggplot(after_vac, aes(x = factor(user_state), y = sentiment)) + geom_boxplot()
-senti_state_after <- aggregate(after_vac$sentiment, list(after_vac$user_state), mean)
+ggplot(after_vac, aes(x = reorder(factor(user_state), sentiment, FUN = median), y = sentiment)) + 
+  geom_boxplot() +
+  coord_flip() +
+  theme_light() +
+  labs(title = "Sentiments After Vaccination by State ", x = "State", y = "Count")
 
-ggplot(tweets_sentiment, aes(x = date, y = Admin_Per_100K)) + geom_point()
-ggplot(tweets_sentiment, aes(x = date, y = Administered_Dose1_Pop_Pct, color = user_state)) + geom_line()
+senti_state_after <- after_vac %>% 
+  group_by(user_state) %>%
+  summarize(mean_sentiment = mean(sentiment),
+            n =  n()) %>%
+  merge(state_names, by.x = "user_state", 
+        by.y = "state_id")
+
+senti_state_after$state_name <- tolower(senti_state_after$state_name)
+
+after_vac %>% 
+  group_by(date) %>%
+  summarize(mean_sentiment = mean(sentiment), n =  n()) %>%
+  ggplot(aes(x = date, y = mean_sentiment)) + 
+  geom_point() + 
+  geom_smooth(span = 0.4)
+
+us_senti_after <- left_join(senti_state_after, us_states, by = c("state_name" = "region"))
+
+ggplot(data = us_senti_after,
+       mapping = aes(x = long, y = lat, group = group, fill = mean_sentiment)) +
+  geom_polygon(color = "gray90", size = 0.1) +
+  coord_map(projection = "albers", lat0 = 39, lat1 = 45) + 
+  scale_fill_gradient2() +
+  labs(title = "Sentiment After Vaccination") + 
+  theme_map() + 
+  labs(fill = "Sentiment")
+
+ggsave("sentiment_after_vaccination.png")
+
+ggplot(tweets_sentiment, aes(x = date, y = admin_per_100k)) + geom_point()
 
 ggplot(before_vac, aes(x = date, y = sentiment, color = user_state)) + geom_point()
-ggplot(after_vac, aes(x = Admin_Per_100K, y = sentiment)) + geom_point()
 
-round(sqrt(var(tweets_sentiment$series_complete_pop_pct)), 2)
+ggplot(after_vac, aes(x = admin_per_100k, y = sentiment)) + geom_point()
+
+
--- 4. Modelling.R
+++ 4. Modelling.R
@@ -3,3 +3,6 @@ library(tidyverse)
 library(lubridate)
 
 # Load the data
+mod1 <- lm(admin_per_100k ~ date + sentiment + new_case + new_death + tot_cases + tot_death, 
+           data = after_vac)
+summary(mod1)
